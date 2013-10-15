e7d6b1bb8a15d5625ac7a4daefb9377d113919a0
Silas Barta <sbarta@gmail.com>
2013-10-15 14:58:24 -0700

    Factor out repeated actions for object creation.

--- src/ggrc/seed/random_prog.py
+++ src/ggrc/seed/random_prog.py
@@ -1,11 +1,36 @@
+import random
+
 from ggrc.app import app, db
 from ggrc.models.all_models import *
 
 prog = Program(title="RandomGenProg", slug="RGP-123")
 
+PREFIX = "EXAMPLE_"
 
-NUM_BASE_OBJECTS = 9
-BASE_OBJECTS = [
+def pick_random(obj_type, num_items=1):
+  all_objs = obj_type.query.all()
+  num_return = min(len(all_objs), num_items)
+  return random.sample(all_objs, num_return)
+
+def create_n_of_each(type_list, num):
+  for obj_type in type_list:
+    for j in xrange(num):
+      num_str = str(j).zfill(3)
+      new_obj = obj_type(
+          title=PREFIX + obj_type.__name__ + num_str,
+          slug=PREFIX + obj_type.__name__.upper() + "-" + num_str,
+      )
+      try:
+        db.session.add(new_obj)
+        db.session.commit()
+      except:
+        print "{0}:{1} already in db".format(
+            obj_type.__name__,
+            new_obj.slug
+        )
+        db.session.rollback()
+
+BIS_OBJECTS = [
     Project,
     Product,
     Process,
@@ -14,29 +39,20 @@ BASE_OBJECTS = [
     Market,
     OrgGroup,
     Facility,
-
+    #Section,
+    Risk,
+]
+GOV_OBJECTS = [
     Control,
     Objective,
     Policy,
     Contract,
     Regulation,
-
-
 ]
 
-for i, obj_type in enumerate(BASE_OBJECTS):
-  for j in xrange(NUM_BASE_OBJECTS):
-    num_str = str(j).zfill(3)
-    new_obj = obj_type(
-        title=obj_type.__name__ + num_str,
-        slug=obj_type.__name__.upper() + "-" + num_str,
-    )
-    try:
-      db.session.add(new_obj)
-      db.session.commit()
-    except:
-      "{0}:{1} already in db".format(obj_type, new_obj.slug)
-      db.session.rollback()
+create_n_of_each(GOV_OBJECTS, 9)
+create_n_of_each(BIS_OBJECTS, 15)
+
 
 print "\n".join(
   [str(
@@ -44,10 +60,8 @@ print "\n".join(
       [(z.title, z.slug) for z in obj_type.query.all()],
       key=lambda x: x[0],
     )
-  ) for obj_type in BASE_OBJECTS]
+  ) for obj_type in (GOV_OBJECTS + BIS_OBJECTS)]
 )
-#ps = Project.query.all()
-#print [p.title for p in ps]
 
 
 
@@ -55,7 +69,6 @@ print "\n".join(
 #    Directive,
 #    Categorization,
 #    ObjectPerson,
-#    Risk,
 #    Category,
 #    DirectiveControl,
 #    ObjectSection,
@@ -67,7 +80,6 @@ print "\n".join(
 #    Event,
 #    ObjectiveControl,
 #    ProgramDirective,
-#    Section,
 #    Option,
 #    SectionObjective,
 #    ControlAssessment,
