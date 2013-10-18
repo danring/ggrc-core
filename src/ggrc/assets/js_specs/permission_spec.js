d47e4485338ed174f9fc2a980d6bc9111b4bbb95
Andy Kant <andy@andykant.com>
2013-10-18 18:03:03 -0500

    Added system roles to unit tests

--- src/ggrc/assets/js_specs/permission_spec.js
+++ src/ggrc/assets/js_specs/permission_spec.js
@@ -20,13 +20,20 @@ describe("Permission", function() {
     , delete_models = create_models.slice(0)
     , roles = {
           Admin: {"__GGRC_ADMIN__": ["__GGRC_ALL__"]}
-        , ProgramOwner: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "create": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "update": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "delete": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"]}
-        , ProgramEditor: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "create": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "update": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "delete": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "ProgramControl", "ProgramDirective", "Relationship"]}
-        , ProgramReader: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "create": [], "update": [], "delete": []}
-        , RoleReader: {"read": ["Role"], "create": [], "update": [], "delete": []}
-        , Reader: {"read": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"]}
-        , ObjectEditor: {"read": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"], "create": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person"], "update": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person"], "delete": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem"]}
-        , ProgramCreator: {"create": ["Program"]}
+        , PrivateProgram: {
+              ProgramOwner: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "create": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "update": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"], "delete": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship", "UserRole"]}
+            , ProgramEditor: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "create": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "update": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "delete": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "ProgramControl", "ProgramDirective", "Relationship"]}
+            , ProgramReader: {"read": ["Cycle", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Program", "ProgramControl", "ProgramDirective", "Relationship"], "create": [], "update": [], "delete": []}  
+          }
+        , System: {
+              RoleReader: {"read": ["Role"], "create": [], "update": [], "delete": []}
+            , Reader: {"read": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"]}
+            , ObjectEditor: {"read": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"], "create": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person"], "update": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person"], "delete": ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem"]}
+            , ProgramCreator: {"create": ["Program"]}
+          }
+      }
+    , role_can = function(role, action, model) {
+        return role[action] && role[action].indexOf(model) > -1;
       }
     , success
     , error
@@ -37,6 +44,7 @@ describe("Permission", function() {
     ; 
 
   beforeEach(function() {
+    jasmine.Clock.useMock();
     unique_id || (unique_id = 1);
     created = [];
 
@@ -153,6 +161,9 @@ describe("Permission", function() {
           xhr.setRequestHeader('X-ggrc-user', JSON.stringify(user));
         }
       }).pipe(function(response) {
+        // Ensure that the next request waits until permissions are safe server-side
+        // !jasmine.Clock.isInstalled() && jasmine.Clock.useMock();
+        // jasmine.Clock.tick(100);
         // Parse permissions from the HTML response
         return GGRC.permissions = JSON.parse(response.match(/GGRC\.permissions.+/)[0].replace(/^GGRC.permissions\s*=\s*(.+?);$/, '$1'));
       });
@@ -165,80 +176,99 @@ describe("Permission", function() {
   
   // Remove every new object that was saved on teardown
   afterEach(function() {
-    // set_permissions("Admin");
+    set_permissions("Admin");
 
-    // // Remove joins first
-    // created.sort(function(a, b) {
-    //   if (a instanceof can.Model.Join && !(b instanceof can.Model.Join)) return -1;
-    //   else if (!(a instanceof can.Model.Join) && b instanceof can.Model.Join) return 1;
-    //   return 0;
-    // });
-
-    // can.each(created, function(instance) {
-    //   instance.id && instance.destroy();
-    // });
-  });
-
-
-  describe("Roles", function() {
-
-    describe("Reader", function() {
-      beforeEach(function() {
-        set_permissions("Reader");
-        reset();
-      });
-
-      describe("can't create", function() {
-        create_models.forEach(function(model) {
-          it(model, function() {
-            if (CMS.Models[model]) {
-              make_model(model).save(success, error);
-              expect(error).toHaveBeenCalled();
-            }
-          });
-        });
-      });
-
-      describe("can read", function() {
-        read_models.forEach(function(model) {
-          it(model, function() {
-            if (CMS.Models[model] && CMS.Models[model].findAll) {
-              CMS.Models[model].findAll({ __stubs_only: true }).done(function(list) {
-                expect(list).not.toBeUndefined();
-              });
-              expect(success).toHaveBeenCalled();
-            }
-          });
-        });
-      });
-
-      describe("can't update or delete", function() {
-        update_models.forEach(function(model) {
-          it(model, function() {
-            if (CMS.Models[model]) {
-              // Create as an admin first
-              set_permissions("Admin");
-              reset();
-              var instance = make_model(model);
-              instance.save(success, error);
-              expect(success).toHaveBeenCalled();
-
-              // Try to update as a reader
-              set_permissions("Reader");
-              reset();
-              instance.save(success, error);
-              expect(error).toHaveBeenCalled();
-
-              // Try to delete as a reader
-              reset();
-              instance.destroy(success, error);
-              expect(error).toHaveBeenCalled();
-            }
-          });
-        });
-      });
-
+    // Remove joins first
+    created.sort(function(a, b) {
+      if (a instanceof can.Model.Join && !(b instanceof can.Model.Join)) return -1;
+      else if (!(a instanceof can.Model.Join) && b instanceof can.Model.Join) return 1;
+      return 0;
     });
 
+    can.each(created, function(instance) {
+      instance.id && instance.destroy();
+    });
+  });
+
+  // Verify CRUD abilities for each role
+  describe("System Roles", function() {
+    for (var role in roles.System) {
+      !function(role) {
+        describe(role, function() {
+          beforeEach(function() {
+            set_permissions(role).done(function() {console.log(arguments)});
+            reset();
+          });
+
+          describe("create", function() {
+            create_models.forEach(function(model) {
+              it((role_can(roles.System[role], "create", model) ? "can" : "can't") + " create " + model, function() {
+                if (CMS.Models[model]) {
+                  make_model(model).save(success, error);
+                  expect(role_can(roles.System[role], "create", model) ? success : error).toHaveBeenCalled();
+                }
+              });
+            });
+          });
+
+          describe("read", function() {
+            read_models.forEach(function(model) {
+              it((role_can(roles.System[role], "read", model) ? "can" : "can't") + " read " + model, function() {
+                if (CMS.Models[model] && CMS.Models[model].findAll) {
+                  CMS.Models[model].findAll({ __stubs_only: true }).done(function(list) {
+                    if (role_can(roles.System[role], "read", model))
+                      expect(list.length).toBeGreaterThan(0);
+                    else
+                      expect(list.length).toEqual(0);
+                  });
+                }
+              });
+            });
+          });
+
+          describe("update", function() {
+            update_models.forEach(function(model) {
+              it((role_can(roles.System[role], "update", model) ? "can" : "can't") + " update " + model, function() {
+                if (CMS.Models[model]) {
+                  // Create as an admin first
+                  set_permissions("Admin");
+                  reset();
+                  var instance = make_model(model);
+                  instance.save(success, error);
+                  expect(success).toHaveBeenCalled();
+
+                  // Try to update
+                  set_permissions(role);
+                  reset();
+                  instance.save(success, error);
+                  expect(role_can(roles.System[role], "update", model) ? success : error).toHaveBeenCalled();
+                }
+              });
+            });
+          });
+
+          describe("delete", function() {
+            delete_models.forEach(function(model) {
+              it((role_can(roles.System[role], "delete", model) ? "can" : "can't") + " delete " + model, function() {
+                if (CMS.Models[model]) {
+                  // Create as an admin first
+                  set_permissions("Admin");
+                  reset();
+                  var instance = make_model(model);
+                  instance.save(success, error);
+                  expect(success).toHaveBeenCalled();
+
+                  // Try to delete
+                  set_permissions(role);
+                  reset();
+                  instance.destroy(success, error);
+                  expect(role_can(roles.System[role], "delete", model) ? success : error).toHaveBeenCalled();
+                }
+              });
+            });
+          });
+        });
+      }(role);
+    }
   });
 });
