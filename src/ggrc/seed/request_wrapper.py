43b3d3dcea925063bd88e14fdfcf2ebc50558716
Silas Barta <sbarta@gmail.com>
2013-10-18 14:03:13 -0700

    Allow prefixes to be passed in CLI call.

--- src/ggrc/seed/request_wrapper.py
+++ src/ggrc/seed/request_wrapper.py
@@ -1,7 +1,11 @@
 import requests
+from sys import argv
 
 if __name__ == "__main__":
   site_domain = "http://localhost:8080"
-  seed_path = "/admin/seedrandom"
+  if len(argv) >= 2:
+    seed_path = "/admin/seedrandom/withprefix/{0}".format(argv[1])
+  else:
+    seed_path = "/admin/seedrandom"
   requests.get(site_domain + "/dashboard")
   requests.get(site_domain + seed_path)
