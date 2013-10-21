8f51350803c67fa78a5323110f34a68198ca081d
Andy Kant <andy@andykant.com>
2013-10-21 17:02:34 -0500

    Reorganizing frontend tests

--- src/ggrc/assets/js_specs/permission_spec.js
+++ src/ggrc/assets/js_specs/permission_spec.js
@@ -1,19 +1,13 @@
 /*!
     Copyright (C) 2013 Google Inc., authors, and contributors <see AUTHORS file>
     Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
-    Created By: brad@reciprocitylabs.com
-    Maintained By: brad@reciprocitylabs.com
+    Created By: andy@reciprocitylabs.com
+    Maintained By: andy@reciprocitylabs.com
 */
 
 describe("Permission", function() {
-  var make_permissions
-    , set_permissions
-    , reset_permissions
-    , current_permissions
-    , last_permissions
-    , make_model
-      // All models except Cycle, since that is unsupported
-    , models = ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"]
+      // Models helpers
+  var models = ["Categorization", "Category", "Control", "ControlControl", "ControlSection", "Cycle", "DataAsset", "Directive", "Contract", "Policy", "Regulation", "DirectiveControl", "Document", "Facility", "Help", "Market", "Objective", "ObjectiveControl", "ObjectControl", "ObjectDocument", "ObjectObjective", "ObjectPerson", "ObjectSection", "Option", "OrgGroup", "PopulationSample", "Product", "ProgramControl", "ProgramDirective", "Project", "Relationship", "RelationshipType", "Section", "SectionObjective", "SystemOrProcess", "System", "Process", "SystemControl", "SystemSystem", "Person", "Program", "Role"]
     , base_instances = {}
     , create_models = models.slice(0).filter(function(model) { return !model.match(/^(Cycle|Category|SystemSystem|SystemControl|Categorization|PopulationSample|Categorization|RelationshipType|Directive|SystemOrProcess|Option)$/) })
     , read_models = models.slice(0).filter(function(model) { return !model.match(/^(Cycle)$/) })
@@ -36,7 +30,208 @@ describe("Permission", function() {
     , role_can = function(role, action, model) {
         return !!role.__GGRC_ADMIN__ || (role[action] && role[action].indexOf(model) > -1);
       }
-    , reset
+
+      // Permissions helpers
+    , current_permissions
+    , last_permissions
+    , make_permissions = function(permissions) {
+        var result = {};
+        for (var action in permissions) {
+          // Context ID w/CRUD
+          if (action == parseInt(action,10)) {
+            !function(context, permissions) {
+              for (var action in permissions) {
+                result[action] = result[action] || {};
+                can.each(permissions[action], function(model) {
+                  result[action][model] = result[action][model] || [];
+                  result[action][model].indexOf(context) === -1 && result[action][model].push(context);
+                });
+              }
+            }(parseInt(action,10), permissions[action]);
+          }
+          // CRUD
+          else {
+            result[action] = result[action] || {};
+            can.each(permissions[action], function(model) {
+              result[action][model] = result[action][model] || [];
+              result[action][model].indexOf(null) === -1 && result[action][model].push(null);
+            });
+          }
+        }
+        return result;
+      }
+    , set_permissions = function(permissions) {
+        var result = {},
+          lookup = function(role) {
+            for (var type in roles) {
+              if (type === role) return roles[type];
+              else {
+                for (var type2 in roles[type]) {
+                  if (type2 === role) return roles[type][type2];
+                }
+              }
+            }
+          };
+        last_permissions = current_permissions;
+        current_permissions = permissions;
+        user = {
+            email: "user_permission@example.com"
+          , name: "Test User"
+          , permissions: make_permissions(typeof permissions === 'string' ? lookup(permissions) : permissions)
+        };
+
+        $.ajax("/login", {
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader('X-ggrc-user', JSON.stringify(user));
+          }
+        }).pipe(function(response) {
+          // Parse permissions from the HTML response
+          return result.permissions = GGRC.permissions = JSON.parse(response.match(/GGRC\.permissions.+/)[0].replace(/^GGRC.permissions\s*=\s*(.+?);$/, '$1'));
+        });
+
+        return function() {
+          result.permissions && reset();
+          return !!result.permissions;
+        };
+      }
+    , reset_permissions = function() {
+        return set_permissions(last_permissions);
+      }
+    , make_model = function(model, deferred, attrs) {
+        deferred = deferred || new $.Deferred();
+        var Model = CMS.Models[model];
+        if (Model) {
+          attrs = can.extend({ context: null }, attrs || {});
+          attrs.name = attrs.title = "permission_" + Date.now() + (++unique_id);
+          if (new Model() instanceof can.Model.Join) {
+            // Use the admin to create temporary objects used in joins
+            runs(function() {
+              waitsFor(set_permissions("Admin"));
+            });
+
+            runs(function() {
+              var def = new $.Deferred();
+
+              // Create temporary objects
+              if (model === 'ObjectSection') {
+                waitsFor(make_model('Section', def));
+                runs(function() {
+                  def.done(function(section) {
+                    var save = jasmine.createSpy();
+                    attrs.section = section;
+                    attrs.section.save(save);
+                    attrs.sectionable = attrs.section.directive;
+                    waitsFor(function() {
+                      return save.callCount;
+                    });
+                  });
+                });
+              }
+              else {
+                can.each(Model.join_keys, function(join, key) {
+                  var def = new $.Deferred();
+                  if (join === CMS.Models.Directive) {
+                    waitsFor(make_model('Regulation', def));
+                    runs(function() {
+                      def.done(function(regulation) {
+                        attrs[key] = regulation;
+                      });
+                    });
+                  }
+                  else if (join === can.Model.Cacheable) {
+                    waitsFor(make_model('Control', def));
+                    runs(function() {
+                      def.done(function(control) {
+                        attrs[key] = control;
+                      });
+                    });
+                  }
+                  else {
+                    waitsFor(make_model(join.shortName, def));
+                    runs(function() {
+                      def.done(function(inst) {
+                        attrs[key] = inst;
+                      });
+                    });
+                  }
+                  runs(function() {
+                    var save = jasmine.createSpy();
+                    attrs[key].save(save);
+                    waitsFor(function() {
+                      return save.callCount;
+                    });
+                  });
+                });
+              }
+
+              runs(function() {
+                waitsFor(reset_permissions());
+              });
+            });
+          }
+          else if (model === "Section") {
+            // Use the admin to create temporary objects used in joins
+            runs(function() {
+              waitsFor(set_permissions("Admin"));
+            });
+
+            runs(function() {
+              var def = new $.Deferred();
+              waitsFor(make_model('Regulation', def));
+              runs(function() {
+                def.done(function(regulation) {
+                  // Create temporary objects
+                  var save = jasmine.createSpy();
+                  attrs.directive = regulation;
+                  attrs.directive.save(save);
+                  waitsFor(function() {
+                    return save.callCount;
+                  });
+                });
+              });
+
+              runs(function() {
+                waitsFor(reset_permissions());
+              });
+            });
+          }
+          else if (model === "Person") {
+            attrs.email = "user_permission_" + Date.now() + (++unique_id) + "@example.com";
+          }
+          else if (model === "Role") {
+            attrs.name = "user_permission_" + Date.now() + (++unique_id);
+          }
+
+          runs(function() {
+            var instance = new Model(attrs);
+            created.push(instance);
+            reset();
+            deferred.resolve(instance);
+          });
+
+          return function() {
+            return deferred.state() === "resolved";
+          };
+        }
+      }
+
+      // Test helpers
+    , reset = function() {
+        // Make everything synchronous
+        $.ajaxSetup({
+            async: true
+          , success: function() {
+              response = arguments[0];
+            }
+          , error: function() {
+              response = arguments[0];
+            }
+        });
+        spyOn($.ajaxSettings, "success").andCallThrough();
+        spyOn($.ajaxSettings, "error").andCallThrough();
+        success = $.ajaxSettings.success;
+        error = $.ajaxSettings.error;
+      }
     , success
     , error
     , success_or_error = function() {
@@ -69,210 +264,6 @@ describe("Permission", function() {
 
     unique_id || (unique_id = 1);
     created = [];
-
-    reset || (reset = function() {
-      // Make everything synchronous
-      $.ajaxSetup({
-          async: true
-        , success: function() {
-            response = arguments[0];
-          }
-        , error: function() {
-            response = arguments[0];
-          }
-      });
-      spyOn($.ajaxSettings, "success").andCallThrough();
-      spyOn($.ajaxSettings, "error").andCallThrough();
-      success = $.ajaxSettings.success;
-      error = $.ajaxSettings.error;
-    });
-
-    // Creates a model instance
-    make_model || (make_model = function(model, deferred, attrs) {
-      deferred = deferred || new $.Deferred();
-      var Model = CMS.Models[model];
-      if (Model) {
-        attrs = can.extend({ context: null }, attrs || {});
-        attrs.name = attrs.title = "permission_" + Date.now() + (++unique_id);
-        if (new Model() instanceof can.Model.Join) {
-          // Use the admin to create temporary objects used in joins
-          runs(function() {
-            waitsFor(set_permissions("Admin"));
-          });
-
-          runs(function() {
-            var def = new $.Deferred();
-
-            // Create temporary objects
-            if (model === 'ObjectSection') {
-              waitsFor(make_model('Section', def));
-              runs(function() {
-                def.done(function(section) {
-                  var save = jasmine.createSpy();
-                  attrs.section = section;
-                  attrs.section.save(save);
-                  attrs.sectionable = attrs.section.directive;
-                  waitsFor(function() {
-                    return save.callCount;
-                  });
-                });
-              });
-            }
-            else {
-              can.each(Model.join_keys, function(join, key) {
-                var def = new $.Deferred();
-                if (join === CMS.Models.Directive) {
-                  waitsFor(make_model('Regulation', def));
-                  runs(function() {
-                    def.done(function(regulation) {
-                      attrs[key] = regulation;
-                    });
-                  });
-                }
-                else if (join === can.Model.Cacheable) {
-                  waitsFor(make_model('Control', def));
-                  runs(function() {
-                    def.done(function(control) {
-                      attrs[key] = control;
-                    });
-                  });
-                }
-                else {
-                  waitsFor(make_model(join.shortName, def));
-                  runs(function() {
-                    def.done(function(inst) {
-                      attrs[key] = inst;
-                    });
-                  });
-                }
-                runs(function() {
-                  var save = jasmine.createSpy();
-                  attrs[key].save(save);
-                  waitsFor(function() {
-                    return save.callCount;
-                  });
-                });
-              });
-            }
-
-            runs(function() {
-              waitsFor(reset_permissions());
-            });
-          });
-        }
-        else if (model === "Section") {
-          // Use the admin to create temporary objects used in joins
-          runs(function() {
-            waitsFor(set_permissions("Admin"));
-          });
-
-          runs(function() {
-            var def = new $.Deferred();
-            waitsFor(make_model('Regulation', def));
-            runs(function() {
-              def.done(function(regulation) {
-                // Create temporary objects
-                var save = jasmine.createSpy();
-                attrs.directive = regulation;
-                attrs.directive.save(save);
-                waitsFor(function() {
-                  return save.callCount;
-                });
-              });
-            });
-
-            runs(function() {
-              waitsFor(reset_permissions());
-            });
-          });
-        }
-        else if (model === "Person") {
-          attrs.email = "user_permission_" + Date.now() + (++unique_id) + "@example.com";
-        }
-        else if (model === "Role") {
-          attrs.name = "user_permission_" + Date.now() + (++unique_id);
-        }
-
-        runs(function() {
-          var instance = new Model(attrs);
-          created.push(instance);
-          reset();
-          deferred.resolve(instance);
-        });
-
-        return function() {
-          return deferred.state() === "resolved";
-        };
-      }
-    });
-
-    // Formats a role/context into a permissions-compatible object
-    make_permissions || (make_permissions = function(permissions) {
-      var result = {};
-      for (var action in permissions) {
-        // Context ID w/CRUD
-        if (action == parseInt(action,10)) {
-          !function(context, permissions) {
-            for (var action in permissions) {
-              result[action] = result[action] || {};
-              can.each(permissions[action], function(model) {
-                result[action][model] = result[action][model] || [];
-                result[action][model].indexOf(context) === -1 && result[action][model].push(context);
-              });
-            }
-          }(parseInt(action,10), permissions[action]);
-        }
-        // CRUD
-        else {
-          result[action] = result[action] || {};
-          can.each(permissions[action], function(model) {
-            result[action][model] = result[action][model] || [];
-            result[action][model].indexOf(null) === -1 && result[action][model].push(null);
-          });
-        }
-      }
-      return result;
-    });
-
-    // Sets a user with the given permissions on the backend
-    set_permissions || (set_permissions = function(permissions) {
-      var result = {},
-        lookup = function(role) {
-          for (var type in roles) {
-            if (type === role) return roles[type];
-            else {
-              for (var type2 in roles[type]) {
-                if (type2 === role) return roles[type][type2];
-              }
-            }
-          }
-        };
-      last_permissions = current_permissions;
-      current_permissions = permissions;
-      user = {
-          email: "user_permission@example.com"
-        , name: "Test User"
-        , permissions: make_permissions(typeof permissions === 'string' ? lookup(permissions) : permissions)
-      };
-
-      $.ajax("/login", {
-        beforeSend: function(xhr) {
-          xhr.setRequestHeader('X-ggrc-user', JSON.stringify(user));
-        }
-      }).pipe(function(response) {
-        // Parse permissions from the HTML response
-        return result.permissions = GGRC.permissions = JSON.parse(response.match(/GGRC\.permissions.+/)[0].replace(/^GGRC.permissions\s*=\s*(.+?);$/, '$1'));
-      });
-
-      return function() {
-        result.permissions && reset();
-        return !!result.permissions;
-      };
-    });
-
-    reset_permissions || (reset_permissions = function() {
-      return set_permissions(last_permissions);
-    });
   });
   
   // Remove every new object that was saved on teardown
